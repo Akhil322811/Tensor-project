2f56ee231e3df23f7683c8c413a9937d06ca535c
Akhil <developerakhil77@gmail.com>
2024-06-18 18:10:14 +0530

    commmiting

--- server/Authentication.js
+++ server/Authentication.js
@@ -2,8 +2,8 @@ const passport = require('passport')
 var GoogleStrategy = require( 'passport-google-oauth2' ).Strategy;
 
 passport.use(new GoogleStrategy({
-    clientID:"643199264313-143dahjm6bfkfd1af1ltugo2u2rl9s1l.apps.googleusercontent.com",
-    clientSecret:"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",
+    clientID:GOOGLE_CLIENT_ID,
+    clientSecret:GOOGLE_CLIENT_SECRET,
     callbackURL: "http://localhost:3333/auth/google/callback",
     passReqToCallback   : true
   },
